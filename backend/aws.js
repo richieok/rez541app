77d35a65281672fec82fee1ad0216868dc7f76b1
AJ <richie.okoro@gmail.com>
2025-11-01 19:36:18 +0100

    Trouble shooting surl issue

--- backend/aws.js
+++ backend/aws.js
@@ -3,6 +3,7 @@ import { SSMClient, GetParametersByPathCommand } from "@aws-sdk/client-ssm"
 const client = new SSMClient({ region: 'us-east-1' });
 
 export async function getParameters(path) {
+    console.log(path)
     try {
         const input = {
             Path: path,
@@ -13,7 +14,7 @@ export async function getParameters(path) {
         const response = await client.send(command);
 
         response.Parameters.forEach(param => {
-            // console.log(`Loading parameter: ${param.Name} with value: ${param.Value}`);
+            console.log(`Loading parameter: ${param.Name} with value: ${param.Value}`);
             const key = param.Name.replace(path, '').toUpperCase();
             process.env[key] = param.Value;
         });
